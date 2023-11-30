3d3d18d9a1a9594509a58609dd1e42fd649ca559
Segni Assaye <segniassaye2@gmail.com>
2023-11-30 12:52:44 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+def add(a, b):
+    """My addition function
 
-if __name__ == "main__":
-        """print the sum of 1 and 2."""
-        from add_0 import add
+    Args:
+        a: 1
+        b: 2
 
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, add(a, b)))
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
