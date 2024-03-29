29dbedc9c4bd886e7652648ff23011b6b58427ee
Segni Assaye <segniassaye2@gmail.com>
2024-03-29 08:39:53 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Takes your GitHub credentials (username and password) and uses the GitHub API
+"""Takes your GitHub credentials (username and password) and uses the GitHub API
 to display your id
 """
 
