2b76f4fbcd2ffda12037af8e1564307bde78c2ce
Segni <segniassaye2@gmail.com>
2024-04-23 13:02:54 +0000

    Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,19 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
-const epId = process.argv[2];
-const apiUrl = `https://swapi.co/api/films/${epId}`;
-request.get(apiUrl, (error, response, body) => {
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const charList = JSON.parse(body).characters;
-    for (let i = 0; i < charList.length; i++) {
-      request.get(charList[i], (err, res, body) => {
-        if (err) {
-          console.log(err);
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
         } else {
-          const names = JSON.parse(body).name;
-          console.log(names);
+          const names = JSON.parse(body);
+          console.log(names.name);
         }
       });
     }
